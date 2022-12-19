f841e5b2f74341baf07cd32ebad5c3aba6be05d1
John4422 <Johnstein456@hotmail.com>
2022-12-19 09:32:51 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace anamation
 {
@@ -8,7 +9,7 @@ namespace anamation
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        SpriteFont words;
         Texture2D tribblegreytexture;
         Texture2D tribblecreamtexture;
         Texture2D tribblebrowntexture;
@@ -22,7 +23,20 @@ namespace anamation
         Vector2 tribblebrownspeed;
         Vector2 tribblecreamspeed;
         Vector2 tribbleorangespeed;
-
+        Texture2D introTexture;
+        Rectangle introRect;
+        MouseState mousestate;
+        Double theta = 2;
+        Double delta;
+        Double posY = 250;
+        Double amp = 100;
+        Screen screen;
+        enum Screen
+        {
+            intro,
+            Tribbleyard, words
+            
+        }
 
         public Game1()
         {
@@ -34,6 +48,8 @@ namespace anamation
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            screen = Screen.intro;
+
             tribbleorRect = new Rectangle(200, 50, 50, 50);
             tribblebroRect = new Rectangle(100, 200, 50, 50);
             tribbleGreRect = new Rectangle (300, 100, 50, 50);
@@ -42,6 +58,7 @@ namespace anamation
             tribblebrownspeed = new Vector2(-1, 2);
             tribblecreamspeed = new Vector2(2, 0);
             tribbleorangespeed = new Vector2(2, 2);
+            introRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight );
             base.Initialize();
         }
 
@@ -53,15 +70,26 @@ namespace anamation
             tribbleorangetexture = Content.Load<Texture2D>("tribbleorange");
             tribblecreamtexture = Content.Load<Texture2D>("tribblecream");
             backgroundtexture = Content.Load<Texture2D>("cloudbackground");
+            introTexture = Content.Load<Texture2D>("Untitled");
+            words = Content.Load<SpriteFont>("Words");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mousestate = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            this.Window.Title = $"mouse x: {mousestate.X}, mouse y: {mousestate.Y}";
+            if(screen == Screen.intro)
+            {
+                if (mousestate.LeftButton == ButtonState.Pressed)
+                    screen = Screen.Tribbleyard;
+            }
+            else if (screen == Screen.Tribbleyard)
+            {
             tribbleGreRect.X += (int)tribbleGreyspeed.X;
             if (tribbleGreRect.Right > 800 || tribbleGreRect.X < 0 )
                 tribbleGreyspeed.X *= -1;
@@ -98,6 +126,8 @@ namespace anamation
             if (tribbleorRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleorRect.Y < 0)
                 tribbleorangespeed.Y *= -1;
             base.Update(gameTime);
+            }
+          
         }
 
         protected override void Draw(GameTime gameTime)
@@ -107,12 +137,25 @@ namespace anamation
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(backgroundtexture, new Rectangle(0, 0, 800, 500), Color.White);
-            _spriteBatch.Draw(tribblegreytexture, tribbleGreRect, Color.White);
-            _spriteBatch.Draw(tribblebrowntexture, tribblebroRect, Color.White);
-            _spriteBatch.Draw(tribblecreamtexture, tribblecreRect, Color.White);
-            _spriteBatch.Draw(tribbleorangetexture, tribbleorRect, Color.White);
-
+            if (screen == Screen.intro)
+            {
+                _spriteBatch.Draw(introTexture,introRect, Color.White);
+                delta = (float) amp * Math.Sin(theta);
+                float d = (float) delta;
+                float p = (float)posY;
+                theta = theta + .1;
+                _spriteBatch.DrawString(words, "Welcome to the bouncing", new Vector2(210, p + d), Color.Black);
+                _spriteBatch.DrawString(words, "Welcome to the bouncing", new Vector2(212, p + d), Color.White);
+            }
+            else if (screen == Screen.Tribbleyard)
+            {
+                _spriteBatch.Draw(backgroundtexture, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+                _spriteBatch.Draw(tribblegreytexture, tribbleGreRect, Color.White);
+                _spriteBatch.Draw(tribblebrowntexture, tribblebroRect, Color.White);
+                _spriteBatch.Draw(tribblecreamtexture, tribblecreRect, Color.White);
+                _spriteBatch.Draw(tribbleorangetexture, tribbleorRect, Color.White);
+            }
+          
             _spriteBatch.End();
             base.Draw(gameTime);
         }
